e3ea52114dec53a2f0029f23f4d72afdf112886d
NishiOwO <nishi@nishi.boats>
2025-11-16 10:03:10 +0900

    at least window

--- mdm/login.c
+++ mdm/login.c
@@ -3,27 +3,46 @@
 #include <Mw/Milsko.h>
 
 void make_login_window(void){
-	MwWidget w, pic, userlabel, usercombo, passlabel, passentry;
+	MwWidget window, pic, userlabel, usercombo, passlabel, passentry, mainsep, sesslabel, sesscombo, fieldsep, shutdown, reboot, ok;
 
 	MwLibraryInit();
 	
-	w = MwVaCreateWidget(MwWindowClass, "login", NULL, (x_width() - 366) / 2, (x_height() - 183) / 2, 366, 183,
+	window = MwVaCreateWidget(MwWindowClass, "login", NULL, (x_width() - 366) / 2, (x_height() - 183) / 2, 366, 183,
 		MwNhasBorder, 1,
 		MwNinverted, 0,
 	NULL);
-	pic = MwVaCreateWidget(MwImageClass, "image", w, 10, 10, 80, 137,
+	pic = MwVaCreateWidget(MwImageClass, "image", window, 10, 10, 80, 137,
 		MwNhasBorder, 1,
 	NULL);
-	userlabel = MwVaCreateWidget(MwLabelClass, "userlabel", w, 95, 10, MwTextWidth(w, "User:"), 16,
+	userlabel = MwVaCreateWidget(MwLabelClass, "userlabel", window, 95, 10, MwTextWidth(window, "User:"), 16,
 		MwNtext, "User:",
 	NULL);
-	usercombo = MwCreateWidget(MwComboBoxClass, "usercombo", w, 95, 10+16+5, 265, 18);
-	passlabel = MwVaCreateWidget(MwLabelClass, "passlabel", w, 95, 10+16+5+18+5, MwTextWidth(w, "Password:"), 16,
+	usercombo = MwCreateWidget(MwComboBoxClass, "usercombo", window, 95, 10+16+5, 265, 18);
+	passlabel = MwVaCreateWidget(MwLabelClass, "passlabel", window, 95, 10+16+5+18+5, MwTextWidth(window, "Password:"), 16,
 		MwNtext, "Password:",
 	NULL);
-	passentry = MwVaCreateWidget(MwEntryClass, "passentry", w, 95, 10+16+5+18+5+16+5, 265, 18,
+	passentry = MwVaCreateWidget(MwEntryClass, "passentry", window, 95, 10+16+5+18+5+16+5, 265, 18,
 		MwNhideInput, 1,
 	NULL);
+	mainsep = MwVaCreateWidget(MwSeparatorClass, "mainsep", window, 95, 10+16+5+18+5+16+5+18, 265, 10,
+		MwNorientation, MwHORIZONTAL,
+	NULL);
+	sesslabel = MwVaCreateWidget(MwLabelClass, "sesslabel", window, 95, 10+16+5+18+5+16+5+18+10, MwTextWidth(window, "Session Type:"), 16,
+		MwNtext, "Session Type:",
+	NULL);
+	sesscombo = MwCreateWidget(MwComboBoxClass, "sesscombo", window, 95, 10+16+5+18+5+16+5+18+10+16+5, 265, 18);
+	fieldsep = MwVaCreateWidget(MwSeparatorClass, "fieldsep", window, (366 - 362) / 2, 10+137, 362, 10,
+		MwNorientation, MwHORIZONTAL,
+	NULL);
+	shutdown = MwVaCreateWidget(MwButtonClass, "shutdown", window, 10, 10+137+10, 80, 18,
+		MwNtext, "Shutdown",
+	NULL);
+	reboot = MwVaCreateWidget(MwButtonClass, "reboot", window, 10+80+5, 10+137+10, 70, 18,
+		MwNtext, "Reboot",
+	NULL);
+	ok = MwVaCreateWidget(MwButtonClass, "ok", window, 366 - 10 - 60, 10+137+10, 60, 18,
+		MwNtext, "OK",
+	NULL);
 
-	MwLoop(w);
+	MwLoop(window);
 }
