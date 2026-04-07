914c00fc53fa2790f34c5284b350b87db7abcceb
NishiOwO <nishi@nishi.boats>
2026-04-07 22:59:34 +0900

    nvm

--- mdm/login.c
+++ mdm/login.c
@@ -257,7 +257,6 @@ void login_window(void) {
 		while(MwPending(root)) {
 			if((s = MwStep(root)) != 0) break;
 		}
-		MwAfterStep(root);
 		pthread_mutex_unlock(&xmutex);
 		if(s != 0) break;
 
--- milkwm/wm.c
+++ milkwm/wm.c
@@ -63,7 +63,6 @@ void loop_wm(void) {
 		while(MwPending(root)) {
 			if((s = MwStep(root)) != 0) break;
 		}
-		MwAfterStep(root);
 		pthread_mutex_unlock(&xmutex);
 		if(s != 0) break;
 
